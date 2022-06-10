fc4f531053a162bf960af2796b457b518e61abc5
Андрей <andybro17@yandex.ru>
2022-06-10 13:05:31 +0200

    Initial commit

---
+++ src/ru/skypro/Main.java
@@ -0,0 +1,20 @@
+package ru.skypro;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int age = 7;
+        if (age == 18) {
+            System.out.println("С окончанием школы!");
+        } else if (age == 21) {
+            System.out.println("Теперь тебе можно пить алкоголь");
+        } else if (age == 7) {
+            System.out.println("Иди в школу");
+        } else {
+            System.out.println("Пока сказать нечего");
+        }
+
+            }
+        }
+
+
